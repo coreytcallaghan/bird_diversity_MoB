57f0e4e7fa995d24e8a6ff1acf56a204a1b51756
CTC <callaghan.corey.t@gmail.com>
2021-02-03 08:56:28 +0100

    refiltering data

--- R/get_initial_ebird_data_from_bigquery.R
+++ R/get_initial_ebird_data_from_bigquery.R
@@ -20,8 +20,8 @@ ebird <- tbl(con, 'ebird_qa_may_2020')
 # with strict filters
 ## extract data
 example_dat <- ebird %>%
-  dplyr::filter(OBSERVATION_DATE > "2015-01-01") %>%
-  dplyr::filter(OBSERVATION_DATE < "2017-12-31") %>%
+  dplyr::filter(OBSERVATION_DATE >= "2015-01-01") %>%
+  dplyr::filter(OBSERVATION_DATE <= "2017-12-31") %>%
   dplyr::select(SAMPLING_EVENT_IDENTIFIER, COMMON_NAME, SCIENTIFIC_NAME, OBSERVATION_COUNT,
                 LOCALITY_ID, LATITUDE, LONGITUDE, OBSERVATION_DATE, CATEGORY, BCR_CODE,
                 TIME_OBSERVATIONS_STARTED, OBSERVER_ID, PROTOCOL_TYPE, DURATION_MINUTES, 
@@ -43,18 +43,43 @@ lists_with_x <- example_dat %>%
 dat2 <- example_dat %>%
   dplyr::filter(!SAMPLING_EVENT_IDENTIFIER %in% lists_with_x$SAMPLING_EVENT_IDENTIFIER)
 
-# filter data to only one unique checklist per locality ID
-lists_to_keep <- dat2 %>%
-  dplyr::select(SAMPLING_EVENT_IDENTIFIER, LOCALITY_ID) %>%
-  distinct() %>%
-  group_by(LOCALITY_ID) %>%
+# # filter data to only one unique checklist per locality ID
+# lists_to_keep <- dat2 %>%
+#   dplyr::select(SAMPLING_EVENT_IDENTIFIER, LOCALITY_ID) %>%
+#   distinct() %>%
+#   group_by(LOCALITY_ID) %>%
+#   sample_n(1)
+
+# filter data to just the continental United States
+dat3 <- dat2 %>%
+  dplyr::filter(COUNTRY=="United States") %>%
+  dplyr::filter(!STATE_CODE %in% c("US-AK", "US-HI"))
+
+# only keep one checklist (randomly) when there is a group identifier
+checklists_to_keep <- dat3 %>%
+  dplyr::select(GROUP_IDENTIFIER, SAMPLING_EVENT_IDENTIFIER) %>%
+  group_by(GROUP_IDENTIFIER) %>%
   sample_n(1)
 
-dat3 <- dat2 %>%
-  dplyr::filter(SAMPLING_EVENT_IDENTIFIER %in% lists_to_keep$SAMPLING_EVENT_IDENTIFIER) %>%
-  replace_na(list(GROUP_IDENTIFIER="keep")) %>%
-  dplyr::filter(GROUP_IDENTIFIER=="keep") %>%
-  dplyr::select(-GROUP_IDENTIFIER, -EFFORT_DISTANCE_KM, -EFFORT_AREA_HA, -NUMBER_OBSERVERS, -CATEGORY) 
+dat4 <- dat3 %>%
+  dplyr::filter(SAMPLING_EVENT_IDENTIFIER %in% checklists_to_keep$SAMPLING_EVENT_IDENTIFIER) %>%
+  bind_rows(dat3 %>%
+              dplyr::filter(is.na(GROUP_IDENTIFIER))) %>%
+  dplyr::select(-GROUP_IDENTIFIER, -EFFORT_DISTANCE_KM, -EFFORT_AREA_HA, -NUMBER_OBSERVERS, -CATEGORY) %>%
+  mutate(OBSERVATION_COUNT=as.numeric(as.character(OBSERVATION_COUNT)))
 
-saveRDS(dat3, "birds/Data/ebird_data_raw.RDS")
+# dat3 <- dat2 %>%
+#   dplyr::filter(SAMPLING_EVENT_IDENTIFIER %in% lists_to_keep$SAMPLING_EVENT_IDENTIFIER) %>%
+#   replace_na(list(GROUP_IDENTIFIER="keep")) %>%
+#   dplyr::filter(GROUP_IDENTIFIER=="keep") %>%
+#   dplyr::select(-GROUP_IDENTIFIER, -EFFORT_DISTANCE_KM, -EFFORT_AREA_HA, -NUMBER_OBSERVERS, -CATEGORY) 
+
+saveRDS(dat4, "Data/ebird_data_raw.RDS")
+
+# filter to breeding season only
+dat5 <- dat4 %>%
+  mutate(MONTH=month(OBSERVATION_DATE, label=TRUE, abbr=TRUE)) %>%
+  dplyr::filter(MONTH %in% c("May", "Jun", "Jul", "Aug"))
+
+saveRDS(dat5, "Data/ebird_data_breeding_season_only.RDS")
 
