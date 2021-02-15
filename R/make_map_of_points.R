626d814ed03e978ceb781b9635bd9f3bdc7163ff
CTC <callaghan.corey.t@gmail.com>
2021-02-15 16:24:23 +0100

    summarizing maps of points and sample sizes

--- R/make_map_of_points.R
+++ R/make_map_of_points.R
@@ -17,7 +17,17 @@ ebird_data <- readRDS("Data/ebird_data_raw_May.RDS") %>%
   left_join(., read_csv("Data/checklists_mod_scores/ebird_samples_mod_scores.csv") %>%
               dplyr::select(first, SAMPLIN) %>%
               rename(ghm=first) %>%
-              rename(SAMPLING_EVENT_IDENTIFIER=SAMPLIN), by="SAMPLING_EVENT_IDENTIFIER")
+              rename(SAMPLING_EVENT_IDENTIFIER=SAMPLIN), by="SAMPLING_EVENT_IDENTIFIER") %>%
+  left_join(., read_csv("Data/Clements-Checklist-v2019-August-2019.csv") %>%
+              dplyr::filter(category=="species") %>%
+              dplyr::select(category, `English name`, `scientific name`, order, family) %>%
+              rename(COMMON_NAME=`English name`,
+                     SCIENTIFIC_NAME=`scientific name`)) %>%
+  dplyr::filter(!family %in% c("Strigidae (Owls)", "Tytonidae (Barn-Owls)",
+                               "Stercorariidae (Skuas and Jaegers)", "Alcidae (Auks, Murres, and Puffins)",
+                               "Sulidae (Boobies and Gannets)", "Procellariidae (Shearwaters and Petrels)",
+                               "Hydrobatidae (Northern Storm-Petrels)", "Oceanitidae (Southern Storm-Petrels)")) %>%
+  dplyr::filter(complete.cases(BCR_CODE))
 
 points_sf <- ebird_data %>%
   dplyr::select(LATITUDE, LONGITUDE, SAMPLING_EVENT_IDENTIFIER, ghm) %>%
@@ -37,7 +47,29 @@ ggplot()+
   scale_color_viridis_c(option="inferno")+
   xlim(130, 60)+
   ylim(25, 50)+
-  theme(legend.position="bottom")
+  theme(legend.position="bottom")+
+  theme(axis.text=element_text(color="black"))
 
 ggsave("Figures/map_of_points_in_USA.png", height=4.3, width=5.4, units="in")
 
+bcrs <- st_read("Data/Spatial data/bcr_terrestrial_shape/BCR_Terrestrial_master.shp")
+
+bcr_summary <- ebird_data %>%
+  group_by(BCR_CODE) %>%
+  summarize(N=length(unique(SAMPLING_EVENT_IDENTIFIER))) %>%
+  dplyr::filter(BCR_CODE != 0) %>%
+  rename(BCR=BCR_CODE)
+
+bcr_plot_dat <- bcrs %>%
+  left_join(., bcr_summary, by="BCR") %>%
+  dplyr::filter(COUNTRY=="USA") %>%
+  dplyr::filter(!PROVINCE_S %in% c("ALASKA", "HAWAIIAN ISLANDS"))
+
+ggplot()+
+  geom_sf(data=bcr_plot_dat, aes(fill=log10(N)))+
+  theme_bw()+
+  theme(panel.grid.major=element_blank())+
+  scale_fill_viridis_c(option="inferno")+
+  theme(axis.text=element_text(color="black"))
+
+ggsave("Figures/map_of_bcrs_and_sample_size.png", height=4.3, width=5.4, units="in")
